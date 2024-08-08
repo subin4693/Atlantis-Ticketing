506a82268f40c11d2e2625df2903a96004d2581a
subin <83167647+subin4693@users.noreply.github.com>
2024-08-08 14:46:51 +0530

    Update UserLayout.jsx

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -9,7 +9,11 @@ function UserLayout() {
         <main className="flex cursor-default min-h-screen">
             <div className="w-full   flex flex-col">
                 <Navbar person={"User"} />
-                <div className="flex-grow p-5 sm:p-10 md:20">
+                 <div
+                    className="flex-grow p-5 sm:p-10 md:20
+bg-[url('https://i0.wp.com/974qa.net/wp-content/uploads/2024/05/Atlantis_The_Immersive_Odyssey_Msheireb_Galleria_Doha_Qatar_2024.jpg?fit=1920%2C1080')] bg-cover bg-center
+                "
+                >
                     <Outlet />
                 </div>
                 <Footer />
